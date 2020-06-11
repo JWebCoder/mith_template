ac4bd3348284607ac7c019078772e173cc804a81
João Moura <joao.moura@b-i.com>
2020-06-12 00:46:29 +0100

    Locked cors version

--- deps.ts
+++ deps.ts
@@ -3,4 +3,4 @@ export { Mith, Request, Response, NextFunction } from  'https://deno.land/x/mith
 export { Router } from 'https://deno.land/x/mith_router@v0.1.1/mod.ts'
 export { serveStatic } from 'https://deno.land/x/mith_static@v0.0.1/mod.ts'
 export { resolve } from "https://deno.land/std@0.53.0/path/mod.ts"
-export { mithCors } from 'https://deno.land/x/cors/mithCors.ts'
+export { mithCors } from 'https://deno.land/x/cors@691c07f24d802ea9a2b193e1c23c4c3f52657996/mithCors.ts'
