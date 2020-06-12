e84118b65043cde2da5a6d7d8163dacfabd03436
João Moura <joao.moura@b-i.com>
2020-06-12 01:12:31 +0100

    Updates deps

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
 export { default as debug } from 'https://deno.land/x/debuglog@345a3c55de4fc0c48bef9e94e81dbd18ac699730/debug.ts'
 export { Mith, Request, Response, NextFunction } from  'https://deno.land/x/mith@v0.7.0/mod.ts'
-export { Router } from 'https://deno.land/x/mith_router@v0.1.1/mod.ts'
+export { Router } from 'https://deno.land/x/mith_router@v0.2.0/mod.ts'
 export { serveStatic } from 'https://deno.land/x/mith_static@v0.0.1/mod.ts'
 export { resolve } from "https://deno.land/std@0.53.0/path/mod.ts"
 export { mithCors } from 'https://deno.land/x/cors@691c07f24d802ea9a2b193e1c23c4c3f52657996/mithCors.ts'
